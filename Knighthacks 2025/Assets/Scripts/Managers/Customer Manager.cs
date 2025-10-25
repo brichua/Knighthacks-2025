4c56fed45ce88fd80c8bd31e6782ccd7cc28716e
Bri <ms.brianna.chua@gmail.com>
2025-10-25 02:23:08 -0400

    Siddu pushing CustomerManager from Bri Account

--- Knighthacks 2025/Assets/Scripts/Managers/Customer Manager.cs
+++ Knighthacks 2025/Assets/Scripts/Managers/Customer Manager.cs
@@ -1,16 +1,54 @@
+using System.Timers;
 using UnityEngine;
 
 public class CustomerManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Customer[] customers;
+    static Timer customerSpawnTimer;
+    bool spawnRequested = false;
+    int anomalyChance = 10;
+    string[] pastryTypes = { "cookie", "cake", "cheese", "cracker" };
+    string[] flowerTypes = { "rose", "bluebell", "daisy" };
+    string[] drinkTypes = { "black", "green", "oolong" };
+
     void Start()
     {
-        
+        double interval = Random.Range(20000, 40000);
+        customerSpawnTimer = new Timer(interval);
+        customerSpawnTimer.Elapsed += (s, e) => { spawnRequested = true; };
+        customerSpawnTimer.Start();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (spawnRequested)
+        {
+            spawnRequested = false;
+            SpawnCustomer();
+            double interval = Random.Range(20000, 40000);
+            customerSpawnTimer = new Timer(interval);
+            customerSpawnTimer.Elapsed += (s, e) => { spawnRequested = true; };
+            customerSpawnTimer.Start();
+        }
+    }
+
+    void SpawnCustomer()
+    {
+        int snackRoll = Random.Range(0, pastryTypes.Length);
+        int flowerRoll = Random.Range(0, flowerTypes.Length);
+        int drinkRoll = Random.Range(0, drinkTypes.Length);
+        string snack = flowerTypes[flowerRoll] + " " + drinkTypes[drinkRoll];
+        string[] choices = { snack, pastryTypes[snackRoll] };
+        if (Random.Range(0, anomalyChance) <= 1) {
+            // idk evil customer
+            anomalyChance = 10;
+            Customer customer = new Customer(choices);
+            customer.updateHallucination();
+        }
+        else {
+            // normal customer
+            anomalyChance -= 2;
+            Customer customer = new Customer(choices);
+        }
     }
 }
