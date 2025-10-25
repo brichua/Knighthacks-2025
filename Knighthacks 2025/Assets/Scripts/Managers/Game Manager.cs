c3aeea3bec581b9cc95ab5444f39e345c9427fb3
Quantomato <juansebastianlomanto@yahoo.com>
2025-10-25 00:42:19 -0400

    Push Scripts for Siddu.

---
+++ Knighthacks 2025/Assets/Scripts/Managers/Game Manager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    //This script will mainly be focused on making sure all of the other scripts can work
+    //together 
+    public MonoBehaviour anomalyManager;
+
+
+
+
+
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
